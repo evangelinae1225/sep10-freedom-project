f4128e15f828eda1de48d7b3833a3ef3e9b0b312
Evangelina <evangelinae2@nycstudents.net>
2026-03-03 14:33:24 +0000

    continue playing around

--- tool/react-app/src/components/ListGroup.tsx
+++ tool/react-app/src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 function ListGroup() {
-    return <ul className="list-group">
-  <li className="list-group-item">An Item </li>
-  <li className="list-group-item">A second item</li>
-  <li className="list-group-item">A third item</li>
-  <li className="list-group-item">A fourth item</li>
-  <li className="list-group-item">And a fifth one</li>
-</ul> }
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-    export default ListGroup;
+  return (
+    <>
+      <ul className="ListGroup">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
-     
+export default ListGroup;
