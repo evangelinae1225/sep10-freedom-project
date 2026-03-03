6ff94e5f7fc1d4b88e1139cd3d5968317981751d
Evangelina <evangelinae2@nycstudents.net>
2026-03-03 14:46:10 +0000

    idk what im doing

--- tool/react-app/src/components/ListGroup.tsx
+++ tool/react-app/src/components/ListGroup.tsx
@@ -1,11 +1,19 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
+
+  const getMessage = () => {
+   return items.length ===0? <p> No items found.</p> : null}
+
+  
   return (
     <>
+      <h1>List Group</h1>
+      {getMessage()}
       <ul className="ListGroup">
         {items.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </>
